4d562ab760437036508e0e7182f7dffb737523a3
Francois Ripault <francois.ripault@epita.fr>
2013-11-21 19:30:03 +0100

    Add a fourth channel to Color; it contains the maximum value for a color. This makes it easier to take into account the take were rays do not hit anything.

--- src/color.cc
+++ src/color.cc
@@ -5,8 +5,9 @@ Color operator+ (Color a, Color other)
   double new_r = a.r()+ other.r();
   double new_g = a.g()+ other.g();
   double new_b = a.b()+ other.b();
+  double new_max = a.max() + other.max();
 
-  return Color(new_r, new_g, new_b);
+  return Color(new_r, new_g, new_b, new_max);
 }
 
 Color operator+(Color rgb, double val)
@@ -18,9 +19,10 @@ Color operator+(Color rgb, double val)
   double new_g = rgb.g()+val;
   double new_b = rgb.b()+val;
 
-  return Color((new_r > 1. ? 1. : new_r)
-              ,(new_g > 1. ? 1. : new_g)
-              ,(new_b > 1. ? 1. : new_b));
+  return Color( (new_r > 1. ? 1. : new_r)
+              , (new_g > 1. ? 1. : new_g)
+              , (new_b > 1. ? 1. : new_b)
+              , rgb.max());
 }
 
 Color operator-(Color rgb, double val)
@@ -32,7 +34,8 @@ Color operator-(Color rgb, double val)
   double new_b = rgb.b()-val;
   return Color((new_r < 0. ? 0. : new_r)
               ,(new_g < 0. ? 0. : new_g)
-              ,(new_b < 0. ? 0. : new_b));
+              ,(new_b < 0. ? 0. : new_b)
+              , rgb.max()); // FIXME: put this max ?
 }
 
 Color operator* (Color a, Color other)
@@ -40,13 +43,13 @@ Color operator* (Color a, Color other)
   double new_r = a.r()* other.r();
   double new_g = a.g()* other.g();
   double new_b = a.b()* other.b();
-
-  return Color(new_r, new_g, new_b);
+  double new_max = a.max() * other.max();
+  return Color(new_r, new_g, new_b, new_max);
 }
 
 Color operator* (Color rgb, double val)
 {
-  return Color(rgb.r() * val, rgb.g() * val, rgb.b() * val);
+  return Color(rgb.r() * val, rgb.g() * val, rgb.b() * val, rgb.max());
 }
 
 Color operator*(double val, Color rgb)
@@ -56,5 +59,5 @@ Color operator*(double val, Color rgb)
 
 Color operator/ (Color rgb, double c)
 {
-  return Color(rgb.r() / c, rgb.g()/c, rgb.b()/c);
+  return Color(rgb.r() / c, rgb.g()/c, rgb.b()/c, rgb.max()/c);
 }
--- src/color.hh
+++ src/color.hh
@@ -6,6 +6,10 @@
 // This files describe how a color is represented in our raytracer.
 // For simplicity purposes, color values are represented as a double between
 // 0 and 1
+// We add max_val_ which is the maximum possible parameter in order to simplify
+// normalisation. A created color has a max_val of 1, the result of rgb1 + rgb2
+// has 2; An "empty" color has a max_val_ of 0 (for instance, the color value
+// when a launched ray hit nothing).
 
 class Color
 {
@@ -15,25 +19,30 @@ class Color
       r_ = r/255.;
       g_ = g/255.;
       b_ = b/255.;
+      max_val_ = 1;
     }
-    Color (double r, double g, double b) : r_(r), g_(g), b_(b) { }
-    Color () {} // FIXME: remove ?
+    Color (double r, double g, double b) : r_(r), g_(g), b_(b), max_val_(1) { }
+    Color (double r, double g, double b, double max_val) : r_(r), g_(g), b_(b), max_val_(max_val) { }
+    Color () : r_(0), g_(0), b_(0), max_val_(0) {} // FIXME: remove ?
 
     double r() {return r_;}
     double g() {return g_;}
     double b() {return b_;}
+    double max() {return max_val_;}
 
     // Conversion to the color representation of OpenCV
     cv::Vec3b toBgr()
     {
-      int r = r_ * 255;
-      int g = g_ * 255;
-      int b = b_ * 255;
+      if (max_val_ == 0)
+        return cv::Vec3b(0,0,0);
+      int r = (r_/max_val_) * 255;
+      int g = (g_/max_val_) * 255;
+      int b = (b_/max_val_) * 255;
       return cv::Vec3b(b,g,r);
     }
 
   private:
-    double r_, g_, b_;
+    double r_, g_, b_, max_val_;
 };
 
 // Basic arithmetic operations on colors, implementation in color.cc
