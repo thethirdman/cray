30153153764b687221b465984429e15ba3e754b1
Francois Ripault <francois.ripault@epita.fr>
2013-11-21 19:27:10 +0100

    Use a reference passing style for intersection checking. Patch triangle intersection. Add the shape plane.

--- src/shape.hh
+++ src/shape.hh
@@ -11,7 +11,7 @@ class Shape
   public:
     // Returns the normal to a shape at the point of intersection, or a null
     // pointer otherwise
-    virtual std::pair<cv::Vec3d, double>* intersect(Ray ray) = 0;
+    virtual bool intersect(Ray ray, cv::Vec3d& intersect, double& dist) = 0;
 
     // The normal vector to a shape at the intersection point pt
     virtual cv::Vec3d normal(cv::Vec3d pt) = 0;
@@ -20,17 +20,19 @@ class Shape
     {
       return color_;
     }
+    double refl(void) { return refl_coef_;}
   protected:
-    Shape(Color col) : color_(col) {}
+    Shape(Color col, double refl) : color_(col), refl_coef_(refl) {}
     Color color_;
+    double refl_coef_;
 };
 
 class Sphere : public Shape
 {
   public:
-    Sphere(cv::Vec3i c, Color color, int r) : center_(c), Shape(color), radius_(r) {}
+    Sphere(cv::Vec3d c, Color color, double r, double refl) : center_(c), Shape(color, refl), radius_(r) {}
 
-    std::pair<cv::Vec3d, double>* intersect(Ray ray)
+    bool intersect(Ray ray, cv::Vec3d& intersect, double& dist)
     {
       double a = ray.dir().dot(ray.dir());
       cv::Vec3d o_c = ray.orig() - center_; // O-C
@@ -59,11 +61,12 @@ class Sphere : public Shape
 
         // FIXME: for now, only one of the two solutiosn is returned
         // it is the smallest distance. Not sure if this is a good idea
-        cv::Vec3d intersect = ray.orig() + mint * ray.dir();
-        return new std::pair<cv::Vec3d, double>(intersect, mint);
+        intersect = ray.orig() + mint * ray.dir();
+        dist = mint;
+        return true;
       }
       else
-        return NULL;
+        return false;
     }
 
     cv::Vec3d normal(cv::Vec3d pt)
@@ -73,44 +76,81 @@ class Sphere : public Shape
 
   private:
     cv::Vec3d center_;
-    int radius_;
+    double radius_;
 };
 
-class Triangle : public Shape
+class Plane : public Shape
 {
   public:
-    Triangle(cv::Vec3d pt1, cv::Vec3d pt2, cv::Vec3d pt3, Color color)
-      : pt1_(pt1), pt2_(pt2), pt3_(pt3),  Shape(color)
+    Plane(cv::Vec3d pt1, cv::Vec3d dir1, cv::Vec3d dir2, Color color, double refl)
+    : pt1_(pt1), dir1_(dir1), dir2_(dir2), Shape(color, refl)
     {
-      cv::Vec3d e1 = pt2_ - pt1_;
-      cv::Vec3d e2 = pt3_ - pt2_;
-      normal_ = normalize(e1.cross(e2));
+      normal_ = normalize(dir1.cross(dir2));
     }
 
-    std::pair<cv::Vec3d, double>* intersect(Ray ray)
+    bool intersect(Ray ray, cv::Vec3d& intersect, double& dist)
     {
-      // The edges of the triangles
-      cv::Vec3d e1 = pt2_ - pt1_;
-      cv::Vec3d e2 = pt3_ - pt2_;
-      cv::Vec3d e3 = pt1_ - pt3_;
-
       // We first compute the intersection between the ray and the plane in
       // which the triangle is included
       // FIXME: name
       double k = normal_.dot(pt1_);
 
       double t = (k - ray.orig().dot(normal_))/(ray.dir().dot(normal_));
-      cv::Vec3d intersect = ray.orig() + t * ray.dir();
+      intersect = ray.orig() + t * ray.dir();
+      dist = t;
+      if (t > 0)
+        return true;
+      else
+        return false;
+    }
+    cv::Vec3d normal(cv::Vec3d pt)
+    {
+      return normal_;
+    }
 
-      // If it hit the plane, and the intersection point is located whithin the
-      // triangle
-      if (t > 0
-          && (e1.dot(intersect - pt1_) > 0)
-          && (e2.dot(intersect - pt2_) > 0)
-          && (e3.dot (intersect - pt3_) > 0))
-        return new std::pair<cv::Vec3d, double>(intersect, t);
+  protected:
+    cv::Vec3d pt1_;
+    cv::Vec3d dir1_;
+    cv::Vec3d dir2_;
+    cv::Vec3d normal_;
+};
 
-      return NULL;
+class Triangle : public Shape
+{
+  public:
+    Triangle(cv::Vec3d pt1, cv::Vec3d pt2, cv::Vec3d pt3, Color color, double refl)
+      : Shape(color, refl), pt1_(pt1), pt2_(pt2), pt3_(pt3)
+      {
+        normal_ = normalize((pt3_ - pt2_).cross(pt1_ - pt3_));
+      }
+
+
+    bool intersect(Ray ray, cv::Vec3d& intersect, double& dist)
+    {
+      // The edges of the triangles
+      cv::Vec3d e1 = (pt2_ - pt1_);
+      cv::Vec3d e2 = (pt3_ - pt1_);
+
+      cv::Vec3d p = ray.dir().cross(e2);
+      double det = e1.dot(p);
+      double inv_det = 1. / det;
+
+      cv::Vec3d t = ray.orig() - pt1_;
+
+      double u = t.dot(p) * inv_det;
+      if (u < 0 || u > 1)
+        return false;
+
+      cv::Vec3d q = t.cross(e1);
+      double v = ray.dir().dot(q) * inv_det;
+      if (v < 0 || u + v > 1)
+        return false;
+      dist = e2.dot(q) * inv_det;
+      intersect = ray.orig() + dist * ray.dir();
+      if (dist > 0)
+        return true;
+      else
+        return false;
     }
 
     cv::Vec3d normal(cv::Vec3d pt)
