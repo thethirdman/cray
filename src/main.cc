bee5215d4f2628112c4775084170c76443ed905c
Francois Ripault <francois.ripault@epita.fr>
2013-11-22 17:23:51 +0100

    Add a new main example

--- src/main.cc
+++ src/main.cc
@@ -27,37 +27,49 @@ int main(int argc, char** argv)
   int realx = SIZE_FACTOR * x_size;
   int realy = SIZE_FACTOR * y_size;
 
-  Camera* camera = new Camera(cv::Vec3i(0,0,-700), cv::Vec3d(0., 0., 1.), cv::Vec3d(0., 1., 0.)
+  Camera* camera = new Camera(cv::Vec3i(0,0,0), cv::Vec3d(0., 0., 1.), cv::Vec3d(0., 1., 0.)
       , realx, realy);
 
-  Shape* sphere = new Sphere(cv::Vec3i(0,0,280), Color(0, 255, 255), 25);
-  //Ray* ray = new Ray(cv::Vec3i(0,100,0), cv::Vec3b(0,0,1));
+  Shape* sphere = new Sphere(cv::Vec3d(-1,0.25,5), Color(0, 255, 255), 0.5, 0.1);
 
-  //Ray *res = sphere->intersect(*ray);
-  //std::cout << res << std::endl;
-  Shape* sphere1 = new Sphere(cv::Vec3i(0,125,300), Color(255,0,0), 75);
-  Shape* sphere2 = new Sphere(cv::Vec3i(150,50,290), Color(0,255,0), 50);
-  Shape* tri1 = new Triangle(cv::Vec3d(-100,-100,300),
-      cv::Vec3d(-50, -100, 300),
-      cv::Vec3d(-75, -25, 325), Color(255,255,0));
-  Shape* tri2 = new Triangle(
-      cv::Vec3d(-500,500, 0),
-      cv::Vec3d(500,500, 0),
-      cv::Vec3d(0,500, 4000),
-      Color(255,255,255));
+  Shape* sphere1 = new Sphere(cv::Vec3i(1,0,7), Color(255,0,0), 0.75, 0.5);
+  Shape* sphere2 = new Sphere(cv::Vec3i(0,-1,7), Color(0,255,0), 0.50, 0.5);
 
-  std::vector<Shape*>* shapes = new std::vector<Shape*>(5);
-  shapes->at(0) = sphere;
-  shapes->at(1) = sphere1;
-  shapes->at(2) = sphere2;
-  shapes->at(3) = tri1;
-  shapes->at(4) = tri2;
+  Shape* plane   = new Plane(cv::Vec3d(0,    0.75,  0), cv::Vec3d(1,0,0), cv::Vec3d(0,0,1), Color(255,255,255), 0.5);
+  Shape* plane1  = new Plane(cv::Vec3d(-3,   0,     0), cv::Vec3d(0,1,0), cv::Vec3d(0,0,1), Color(255,200,200), 0);
+  Shape* plane2  = new Plane(cv::Vec3d(3,    0,     0), cv::Vec3d(0,1,0), cv::Vec3d(0,0,1), Color(200,255,200), 0);
+  Shape* plane3  = new Plane(cv::Vec3d(0,    0,     10), cv::Vec3d(0,1,0), cv::Vec3d(1,0,0), Color(200,200,255), 0);
+  Shape* plane4  = new Plane(cv::Vec3d(0,   -3,     0), cv::Vec3d(1,0,0), cv::Vec3d(0,0,1), Color(255,255,255), 0);
+  Shape* tri1 = new Triangle(cv::Vec3d(1.80, 0,     7),
+                             cv::Vec3d(1,     0.75, 4),
+                             cv::Vec3d(3,     0.75, 7), Color(255,255,0), 0.5);
+  //Shape* tri2 = new Triangle(
+  //    cv::Vec3d(-10000,5000, 0),
+  //    cv::Vec3d(5000,5000, 0),
+  //    cv::Vec3d(0,5000, 40000),
+  //    Color(255,255,255));
 
-  Light light(cv::Vec3d(0., -200., 100.), Color(255,255,255));
-  //Light light1(cv::Vec3d(200., 0., 200.), Color(255,0,0));
+  std::vector<Shape*>* shapes = new std::vector<Shape*>();
+  shapes->push_back(sphere);
+  shapes->push_back(sphere1);
+  shapes->push_back(sphere2);
+  shapes->push_back(plane);
+  shapes->push_back(tri1);
+  shapes->push_back(plane1);
+  shapes->push_back(plane2);
+  shapes->push_back(plane3);
+  shapes->push_back(plane4);
+
+  Light light(cv::Vec3d(-2.9, -2.9, 9.9), Color(255,255,255));
+  Light light1(cv::Vec3d(-2.9, -2.9, 0.), Color(255,255,255));
+  Light light2(cv::Vec3d(2.9,  -2.9, 9.9), Color(255,255,255));
+  Light light3(cv::Vec3d(2.9,  -2.9, 0.), Color(255,255,255));
+
   std::vector<Light>* lights = new std::vector<Light>(1);
-  lights->at(0) = light;
-  //lights->at(1) = light1;
+  lights->push_back(light);
+  lights->push_back(light1);
+  lights->push_back(light2);
+  lights->push_back(light3);
 
 
   //std::fstream stream(argv[1]);
